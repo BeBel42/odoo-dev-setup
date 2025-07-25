d16f90c917b9173c8064af0c5bee6046c562d3de
Martin (mlef) <mlef@odoo.com>
2025-07-25 17:24:23 +0200

    added pyright + improved setup script

--- setup.bash
+++ setup.bash
@@ -5,32 +5,29 @@
 set -e
 set -u
 
-PROJECT_DIR=$(pwd)
+PROJECT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-[ ! -d "$PROJECT_DIR"/enterprise ] && git clone --depth 1 git@github.com:odoo/enterprise.git
-[ ! -d "$PROJECT_DIR"/odoo ] && git clone --depth 1 git@github.com:odoo/odoo.git
-[ ! -d "$PROJECT_DIR"/tutorials ] && git clone --depth 1 git@github.com:odoo/tutorials.git
+# clone and setup each project
+for i in odoo enterprise tutorials; do
+	echo "Setting up $i project..."
+	[ ! -d "$PROJECT_DIR/$i" ] && git clone --depth 1 "git@github.com:odoo/$i.git"
+	cd "$PROJECT_DIR/$i"
+	git config --global --add safe.directory "$PROJECT_DIR/$i"
+	git remote add dev "git@github.com:odoo-dev/$i.git" || echo "Skipping remote creation"
+	git remote set-url --push origin you_should_not_push_on_this_repository
+	rm -f "$PROJECT_DIR/$i/pyrightconfig.json"
+	ln -s "$PROJECT_DIR/pyrightconfig.json" "$PROJECT_DIR/$i/pyrightconfig.json"
 
-git config --global --add safe.directory "$PROJECT_DIR/odoo"
-git config --global --add safe.directory "$PROJECT_DIR/enterprise"
-git config --global --add safe.directory "$PROJECT_DIR/tutorials"
+	echo "Done setting up $i project..."
+done
 
-cd "$PROJECT_DIR/odoo"
-git remote add dev git@github.com:odoo-dev/odoo.git || echo "Skipping remote creation"
-git remote set-url --push origin you_should_not_push_on_this_repository
+# add odoo symlink for better code completion
+for i in enterprise tutorials; do
+	rm -f "$PROJECT_DIR/$i/odoo" # to avoid symlink bug in ./odoo/odoo/odoo
+	ln -s "$PROJECT_DIR/odoo/odoo" "$PROJECT_DIR/$i/odoo"
+done
 
-cd "$PROJECT_DIR/enterprise"
-git remote add dev git@github.com:odoo-dev/enterprise.git || echo "Skipping remote creation"
-git remote set-url --push origin you_should_not_push_on_this_repository
-rm -f "$PROJECT_DIR/enterprise/odoo" # to avoid symlink bug in ./odoo/odoo/odoo
-ln -s "$PROJECT_DIR/odoo/odoo" "$PROJECT_DIR/enterprise/odoo"
-
-cd "$PROJECT_DIR/tutorials"
-git remote add dev git@github.com:odoo-dev/tutorials.git || echo "Skipping remote creation"
-git remote set-url --push origin you_should_not_push_on_this_repository
-rm -f "$PROJECT_DIR/tutorials/odoo" # to avoid symlink bug in ./odoo/odoo/odoo
-ln -s "$PROJECT_DIR/odoo/odoo" "$PROJECT_DIR/tutorials/odoo"
-
+# create global venv and install dependencies
 cd "$PROJECT_DIR"
 [ ! -d "$PROJECT_DIR/venv" ] && python3 -m venv "$PROJECT_DIR/venv"
 ./venv/bin/python3 -m pip install -r ./odoo/requirements.txt
