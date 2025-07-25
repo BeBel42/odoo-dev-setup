c84d823bb32846306a06498df61fec7e36a42820
Martin Lefevre <contact@mlefevre.xyz>
2025-07-25 19:54:23 +0200

    started run.bash

---
+++ run.bash
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# TODO test if it works (not done yet either)
+
+set -e
+set -d
+
+error() {
+	error_message=$(
+		cat "
+$0 usage:
+	-m | --module <module_name>
+		Select the module to update / install
+	-d | --drop
+		Drop the database to restart an empty session"
+	)
+	echo "$1" 1>&2
+	echo "$error_message" 1>&2
+}
+
+# default values
+drop=0
+install=0
+help=0
+
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+	-m | --module)
+		module="$2"
+		shift
+		;;
+	-d | --drop) drop=1 && install=1 ;;
+	-i | --install) install=1 ;;
+	-h | --help) help=1 ;;
+	*)
+		error "Unknown parameter passed: $1"
+		exit 1
+		;;
+	esac
+	shift
+done
+
+if [[ $help = 1 ]]; then
+	error ""
+	exit 0
+fi
+
+if [ -z $module ]; then
+	error "Unknown parameter passed: $1"
+	exit 1
+fi
+
+echo "Module: $module"
+echo "Should drop: $drop"
+echo "Should -i $install"
+
+docker compose up db
+if [[ $drop = 1 ]]; then
+	docker compose exec db psql 'DROP DATABASE $POSTGRES_DB;'
+fi
+
+action="-u"
+if [[ $install = 1 ]]; then
+	action="-i"
+fi
+
+docker compose down odoo
+docker compose up odoo "$action $module"
