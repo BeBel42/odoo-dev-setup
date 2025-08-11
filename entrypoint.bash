302dd22d40deb729b735d748f4d079f02f432ff7
Martin (mlef) <mlef@odoo.com>
2025-08-11 09:22:44 +0200

    did some refactoring

--- entrypoint.bash
+++ entrypoint.bash
@@ -4,14 +4,16 @@
 # Arguments to this script will be directly passed to odoo-bin
 
 set -e
-set -x
 set -u
 
 cd "$ODOO_DIR/"
+odoo_command="$ODOO_DIR/community/odoo-bin --config $ODOO_DIR/.odoorc $*"
+
+echo -e "\033[0;35m$odoo_command\033[0m"
+
 # using exec to make it receive stop signals
 exec "$ODOO_DIR/venv/bin/python3" \
 	-Xfrozen_modules=off \
 	-m debugpy \
 	--listen 0.0.0.0:5678 \
-	"$ODOO_DIR/community/odoo-bin" \
-	--config "$ODOO_DIR/.odoorc" "$@"
+	$odoo_command
