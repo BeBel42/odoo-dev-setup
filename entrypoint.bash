a1f545bf89ea21ed5e38fe00d3657cd44df92f01
Martin (mlef) <mlef@odoo.com>
2025-08-19 17:15:14 +0200

    changed info to task

--- entrypoint.bash
+++ entrypoint.bash
@@ -10,9 +10,12 @@ odoo_command="$ODOO_DIR/community/odoo-bin --config=$ODOO_DIR/.odoorc $*"
 
 echo -e "\033[0;35m$odoo_command\033[0m"
 
+debug_params=""
+if [[ $ENABLE_DEBUG = 1 ]]; then
+	debug_params="-Xfrozen_modules=off -m debugpy --listen localhost:5678"
+fi
+
+set -x
+
 # using exec to make it receive stop signals
-exec "$ODOO_DIR/venv/bin/python3" \
-	-Xfrozen_modules=off \
-	-m debugpy \
-	--listen localhost:5678 \
-	$odoo_command
+exec "$ODOO_DIR/venv/bin/python3" $debug_params $odoo_command
